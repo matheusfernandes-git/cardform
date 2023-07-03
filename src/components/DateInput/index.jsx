8f6a9f2cb4505fc401d03807cb487ad3f5221638
Matheus Fernandes <100592742+matheusfernandes-git@users.noreply.github.com>
2023-07-03 20:36:54 -0300

    ...

--- src/components/DateInput/index.jsx
+++ src/components/DateInput/index.jsx
@@ -1,14 +1,13 @@
-import styles from "./style.module.css";
-
 export default function DateInput({ placeholder, value, handleValue, label, teste }) {
   const handleChange = (e) => {
     handleValue(e.target.value);
   };
   return (
-    <div className={styles.input_group}>
+    <div>
       <label>{label}</label>
       <div>
         <input
+          style={{ width: '100px' }}
           required
           className={teste}
           type="number"
